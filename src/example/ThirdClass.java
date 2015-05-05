191b928f7ad8dbcd12c9e2d50f7398227d6857b4
Leon B. <leonbornemann@gmx.de>
2015-05-05 15:35:32 +0200

    Hi does this work?

---
+++ src/example/ThirdClass.java
@@ -0,0 +1,12 @@
+package example;
+
+public class ThirdClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("I sure hope gitignore works");
+	}
+
+}
