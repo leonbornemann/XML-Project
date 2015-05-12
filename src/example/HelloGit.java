3d9a3f7c1ee91be3ab0ae1b2a9dd988a8e52b3d1
Leon B. <leonbornemann@gmx.de>
2015-05-12 13:49:55 +0200

    Testcommit

--- src/example/HelloGit.java
+++ src/example/HelloGit.java
@@ -8,5 +8,6 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello git");
 		System.out.println("more output");
+		System.out.println("much more output");
 	}
 }
