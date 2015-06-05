501729e81ef4a635938fc67a60b23fe3fdff8ff0
Leon B. <leonbornemann@gmx.de>
2015-06-05 11:44:26 +0200

    For xslt transformation ( #6 ): 	-added Xalan library 	-added Example transformation 	-added respective new directories

--- resources/xslt stylesheets/test.xslt
+++ resources/xslt stylesheets/test.xslt
@@ -1,6 +1,14 @@
 <?xml version="1.0"?>
 	<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
+		<xsl:template match="child">
+			<neuKind>
+				Kindknoten gefunden
+			</neuKind>
+		</xsl:template>
 		<xsl:template match="root">
-			<neu> Beispieltext </neu>
+			<neuRoot> 
+				Elternknoten gefunden 
+			<xsl:apply-templates/>
+			</neuRoot>
 		</xsl:template>
 	</xsl:stylesheet>
--- src/xslt/Example.java
+++ src/xslt/Example.java
@@ -1,8 +1,11 @@
 package xslt;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
-import javax.xml.transform.Result;
 import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
@@ -12,14 +15,14 @@ import javax.xml.transform.stream.StreamSource;
 
 public class Example {
 
-	public static void main(String[] args) throws TransformerException{
+	public static void main(String[] args) throws TransformerException, IOException{
 		TransformerFactory fac = TransformerFactory.newInstance();
 		String xsltPath = "resources/xslt stylesheets/test.xslt";
 		Source source = new StreamSource(new File(xsltPath));
 		Transformer transformer = fac.newTransformer(source);
 		String xmlSrcPath = "resources/example xml files/input1.xml";
 		String xmlDstPath = "resources/example xml files/output1.xml";
-		Result res = new StreamResult(new File(xmlDstPath));
+		StreamResult res = new StreamResult(new FileWriter(new File(xmlDstPath)));
 		transformer.transform(new StreamSource(new File(xmlSrcPath)), res);
 	}
 }
